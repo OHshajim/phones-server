b087c4aab41438da55644e6017c84c75dbd2ff0b
OHshajim <ajshajimmax@gmail.com>
2024-04-21 07:17:39 +0600

    phone server

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const phones = require('./phones.json');
+const app = express()
+const port = 5000
+
+app.get('/', (req, res) => {
+    res.send('successfully done  ')
+})
+
+app.get('/phones', (req, res) => {
+    res.send(phones)
+})
+
+app.get('/phones/:id', (req, res) => {
+    const id = req.params.id
+    const phone = phones.find(phone => phone.id == id)
+    res.send(phone)
+})
+
+app.listen(port, () => {
+    console.log("your server is successfully opened on : ", port);
+})
